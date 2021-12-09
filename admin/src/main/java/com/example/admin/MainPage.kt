1cd964090ac0424c3d875f231d93703403939698
rahul <rahulcchauhan60@gmail.com>
2021-12-09 15:30:17 +0530

    changes made

--- admin/src/main/java/com/example/admin/MainPage.kt
+++ admin/src/main/java/com/example/admin/MainPage.kt
@@ -1,13 +1,24 @@
 package com.example.admin
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Html
 import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.example.admin.databinding.ActivityMainPageBinding
 
 class MainPage : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
+    private var isLocationPermissionGranted = false
+    private var isCameraPermissionGranted = false
+    private var isSmsPermissionGranted = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.title = Html.fromHtml("<font color='#FFFFFF'>admin</font>")
@@ -16,6 +27,15 @@ class MainPage : AppCompatActivity(), View.OnClickListener {
 
         binding.addEmployee.setOnClickListener(this)
         binding.employeeDetails.setOnClickListener(this)
+
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
+                permission ->
+            isLocationPermissionGranted = permission[Manifest.permission.ACCESS_FINE_LOCATION] ?: isLocationPermissionGranted
+            isCameraPermissionGranted = permission[Manifest.permission.CAMERA] ?: isCameraPermissionGranted
+            isSmsPermissionGranted = permission[Manifest.permission.READ_SMS] ?: isSmsPermissionGranted
+        }
+
+        runtimePermission()
 
     }
 
@@ -33,5 +53,31 @@ class MainPage : AppCompatActivity(), View.OnClickListener {
     override fun onBackPressed() {
         finishAffinity()
         super.onBackPressed()
+    }
+
+    private fun runtimePermission(){
+        isLocationPermissionGranted = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+
+        isCameraPermissionGranted = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+
+        isSmsPermissionGranted = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.READ_SMS) == PackageManager.PERMISSION_GRANTED
+
+        val requests: MutableList<String> = ArrayList()
+
+        if (!isLocationPermissionGranted){
+            requests.add(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+        if (!isCameraPermissionGranted){
+            requests.add(Manifest.permission.CAMERA)
+        }
+        if (!isSmsPermissionGranted){
+            requests.add(Manifest.permission.READ_SMS)
+        }
+        if (requests.isNotEmpty()){
+            permissionLauncher.launch(requests.toTypedArray())
+        }
     }
 }
