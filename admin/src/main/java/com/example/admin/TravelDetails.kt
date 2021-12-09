c0e3c4bbd778a458c825707ed48afc3fd72cdadc
rahul <rahulcchauhan60@gmail.com>
2021-12-09 15:40:28 +0530

    changes made

--- admin/src/main/java/com/example/admin/TravelDetails.kt
+++ admin/src/main/java/com/example/admin/TravelDetails.kt
@@ -1,13 +1,24 @@
 package com.example.admin
 
+import android.content.Context
+import android.location.Location
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Html
 import com.example.admin.databinding.ActivityTravelDetailsBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class TravelDetails : AppCompatActivity() {
+class TravelDetails : AppCompatActivity(),OnMapReadyCallback {
 
     private lateinit var binding: ActivityTravelDetailsBinding
+    private lateinit var locationManager: LocationManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,11 +27,27 @@ class TravelDetails : AppCompatActivity() {
         supportActionBar?.setHomeAsUpIndicator(R.drawable.back)
         binding = ActivityTravelDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         finish()
         return super.onSupportNavigateUp()
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        map.addMarker(
+            MarkerOptions().position(LatLng(21.126110238943085, 79.13540831252685))
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+                .title("It's my current location"))
+        map.animateCamera(
+            CameraUpdateFactory
+                .newLatLngZoom(LatLng(21.126110238943085, 79.13540831252685),16F))
     }
 }
