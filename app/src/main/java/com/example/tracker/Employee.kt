56f30b9ed82e038e80e9fcf2dfcfca87e5aa356c
rahul <rahulcchauhan60@gmail.com>
2021-12-09 15:26:57 +0530

    changes made

--- app/src/main/java/com/example/tracker/Employee.kt
+++ app/src/main/java/com/example/tracker/Employee.kt
@@ -1,25 +1,46 @@
 package com.example.tracker
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Dialog
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Html
 import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.tracker.databinding.ActivityEmployeeBinding
 import com.google.android.material.button.MaterialButton
 
 class Employee : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityEmployeeBinding
+    private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
     private lateinit var dialog: Dialog
+    private var isLocationPermissionGranted = false
+    private var isCameraPermissionGranted = false
+    private var isSmsPermissionGranted = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEmployeeBinding.inflate(layoutInflater)
         supportActionBar?.title = Html.fromHtml("<font color='#FFFFFF'>Employee</font>")
         setContentView(binding.root)
+
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
+            permission ->
+            isLocationPermissionGranted = permission[Manifest.permission.ACCESS_FINE_LOCATION] ?: isLocationPermissionGranted
+            isCameraPermissionGranted = permission[Manifest.permission.CAMERA] ?: isCameraPermissionGranted
+            isSmsPermissionGranted = permission[Manifest.permission.READ_SMS] ?: isSmsPermissionGranted
+        }
+
+        runtimePermission()
 
         val employeeAdapter = ClientListAdapter(this)
         binding.clientList.adapter = employeeAdapter
@@ -54,5 +75,31 @@ class Employee : AppCompatActivity(), View.OnClickListener {
     override fun onBackPressed() {
         finishAffinity()
         super.onBackPressed()
+    }
+
+    private fun runtimePermission(){
+        isLocationPermissionGranted = ContextCompat.checkSelfPermission(this,
+        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+
+        isCameraPermissionGranted = ContextCompat.checkSelfPermission(this,
+        Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+
+        isSmsPermissionGranted = ContextCompat.checkSelfPermission(this,
+        Manifest.permission.READ_SMS) == PackageManager.PERMISSION_GRANTED
+
+        val requests: MutableList<String> = ArrayList()
+
+        if (!isLocationPermissionGranted){
+            requests.add(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+        if (!isCameraPermissionGranted){
+            requests.add(Manifest.permission.CAMERA)
+        }
+        if (!isSmsPermissionGranted){
+            requests.add(Manifest.permission.READ_SMS)
+        }
+        if (requests.isNotEmpty()){
+            permissionLauncher.launch(requests.toTypedArray())
+        }
     }
 }
